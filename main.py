a361d15c708a1467b7aef4635845cfa0a7e5bd56
anagelberg <62032317+anagelberg@users.noreply.github.com>
2023-04-21 16:58:35 -0700

    stylize web and improve user feedback

--- main.py
+++ main.py
@@ -7,36 +7,39 @@ import subprocess
 
 ####################
 encrypter = Encrypter()
-#input_message = input('Enter your message: ').upper().strip()
+# input_message = input('Enter your message: ').upper().strip()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.app_context().push()
 Bootstrap(app)
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         input_message = request.form["input"].upper().strip()
-        if re.search('[^_/. -]', input_message):  # returns true if any characters other than '_, /, ., - are found in the input.
+        if re.search('[^_/. -]',
+                     input_message):  # returns true if any characters other than '_, /, ., - are found in the input.
             encrypter.encrypt_message(input_message)
             if encrypter.error:
-                return render_template("index.html", error=', '.join(encrypter.invalid_letters), type='encrypt')
+                return render_template("index.html", previous_input=input_message, error=', '.join(encrypter.invalid_letters),
+                                       type='encrypt')
             else:
-                return render_template("index.html", type='encrypt', message=encrypter.encrypted_message)
+                return render_template("index.html", type='encrypt', previous_input=input_message,
+                                       message=encrypter.encrypted_message)
         else:
             encrypter.decrypt_message(input_message)
             if encrypter.error:
-                return render_template("index.html", error=', '.join(encrypter.invalid_letters), type='decipher')
+                return render_template("index.html", previous_input=input_message, error=', '.join(encrypter.invalid_letters),
+                                       type='decipher')
             else:
-                return render_template("index.html", type='decipher', message=encrypter.decrypted_message)
-
+                return render_template("index.html", previous_input=input_message, type='decipher',
+                                       message=encrypter.decrypted_message)
 
     return render_template("index.html")
+
 
 subprocess.run('cmd /c start chrome "http://127.0.0.1:5000"')
 if __name__ == '__main__':
     app.run()
-
-
-
